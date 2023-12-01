03e2e37d47d670e7cb25e405a8adbbc09a519c99
Davidnino21 <davidnino_21@yahoo.com>
2023-12-01 15:52:11 -0800

    created server

---
+++ server.js
@@ -0,0 +1,9 @@
+//Created the server 
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => 
+console.log(`App listening at http://localhost:${PORT}`)
+);
