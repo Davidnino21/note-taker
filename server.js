33fd88e94f910c5f70ffaa4fe398b882618adbbd
Davidnino21 <davidnino_21@yahoo.com>
2023-12-01 16:33:49 -0800

    created client routes

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 //Created the server 
 const express = require('express');
+const path = require('path');
 const app = express();
+const apiRoutes = require('./api')
 
 const PORT = process.env.PORT || 3000
+
+app.use(express.static('public'))
+
+app.use(apiRoutes)
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
 
 app.listen(PORT, () => 
 console.log(`App listening at http://localhost:${PORT}`)
